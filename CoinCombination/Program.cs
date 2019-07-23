87ba0db3846794b80fac51ea103feb251b36cac5
Na Hyung Choi and Uriel Gonzalez <na@>
2019-07-23 16:39:47 -0700

    add UI

--- CoinCombination/Program.cs
+++ CoinCombination/Program.cs
@@ -5,6 +5,19 @@ class Program
 {
     private static void Main()
     {
+        Console.WriteLine("Welcome to the Coin Slide!");
+        Console.WriteLine("Please enter your change amount in cents: ");
+        string coinInput = Console.ReadLine();
+        int intInput = int.Parse(coinInput);
+
+        CoinSlide.GetCoins(intInput);
+        Console.WriteLine($"Your coin input is \"{coinInput}\" which equals to: ");
+        Console.WriteLine($"\"{CoinSlide.numQuarters}\" quarters.");
+        Console.Write($"\"{CoinSlide.numDimes}\" dimes.");
+        Console.Write($"\"{CoinSlide.numNickels}\" nickels.");
+        Console.Write($"\"{CoinSlide.numPennies}\" pennies.");
+
+        Main();
 
     }
 }
